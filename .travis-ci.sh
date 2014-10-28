5bce078728fab2cf8e1a202a2336eb81088c9ac2
Hannes Mehnert <hannes@mehnert.org>
2014-10-28 23:27:40 +0100

    use opam 1.2

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,8 +2,8 @@
 OPAM_PACKAGES="cstruct sexplib ctypes nocrypto oUnit"
 
 case "$OCAML_VERSION" in
-    4.01.0) ppa=avsm/ocaml41+opam11 ;;
-    4.02.0) ppa=avsm/ocaml42+opam11 ;;
+    4.01.0) ppa=avsm/ocaml41+opam12 ;;
+    4.02.0) ppa=avsm/ocaml42+opam12 ;;
     *) echo Unknown $OCAML_VERSION; exit 1 ;;
 esac
 
@@ -15,7 +15,7 @@ export OPAMYES=1
 
 opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
 
-opam pin nocrypto git://github.com/mirleft/ocaml-nocrypto.git
+opam pin -n add nocrypto git://github.com/mirleft/ocaml-nocrypto.git
 
 opam install ${OPAM_PACKAGES}
 
