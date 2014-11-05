306ca01650f08778cbda38f2961902cf2199079c
Hannes Mehnert <hannes@mehnert.org>
2014-11-05 19:47:14 +0100

    get rid of handshake_utils

--- src/handshake_utils.ml
+++
@@ -1,8 +0,0 @@
-open State
-
-(* Monadic control-flow core. *)
-type error = string
-include Control.Or_error_make (struct type err = error end)
-exception Handshake_error of error
-let raise_unknown msg = raise (Handshake_error msg)
-
