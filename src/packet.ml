9a42133464dc1579468508cf8084708780979bad
Hannes Mehnert <hannes@mehnert.org>
2014-11-12 15:40:34 +0100

    add tlv_type enum

--- src/packet.ml
+++ src/packet.ml
@@ -7,6 +7,18 @@ cenum message_type {
   SIGNATURE        = 0x12 ;
 } as uint8_t (sexp)
 
+cenum tlv_type {
+  PADDING = 0 ;
+  DISCONNECTED = 1 ;
+  SMP_MESSAGE_1 = 2 ;
+  SMP_MESSAGE_2 = 3 ;
+  SMP_MESSAGE_3 = 4 ;
+  SMP_MESSAGE_4 = 5 ;
+  SMP_ABORT = 6 ;
+  SMP_MESSAGE_1Q = 7 ;
+  EXTRA_SYMMETRIC_KEY = 8
+} as uint16_t (sexp)
+
 let int_of_version = function
   | `V2 -> 2
   | `V3 -> 3
