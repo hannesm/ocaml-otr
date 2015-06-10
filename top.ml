81fdc064aa0a38fab2c98dc40ecb51ea88f86cff
Hannes Mehnert <hannes@mehnert.org>
2015-06-10 17:03:53 +0100

    require stringext

--- top.ml
+++ top.ml
@@ -1,6 +1,6 @@
 (* #use this to setup the interactive environment. *)
 
-#require "cstruct, cstruct.syntax, sexplib.syntax, nocrypto";;
+#require "cstruct, cstruct.syntax, sexplib.syntax, nocrypto, stringext";;
 
 #directory "_build/src";;
 #load_rec "otr.cmo";;
