d714d2a9950ab3f64484f116fc2ddb435ac01643
Hannes Mehnert <hannes@mehnert.org>
2015-06-26 09:46:37 +0100

    adapt to Counter changes in nocrypto

--- src/crypto.ml
+++ src/crypto.ml
@@ -60,8 +60,7 @@ let data_keys data high =
   let recvmac = Hash.digest `SHA1 recvaes in
   (sendaes, sendmac, recvaes, recvmac)
 
-module Counter = Cipher_block.Counters.Inc_BE
-module AES_CTR = Cipher_block.AES.CTR (Counter)
+module AES_CTR = Cipher_block.AES.CTR
 
 let crypt ~key ~ctr msg =
   let ctr =
