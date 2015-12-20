5c903bc94b687d22da346c517db9f89af3f80206
Hannes Mehnert <hannes@mehnert.org>
2015-12-20 17:53:56 +0100

    moved away from stringext, depend on astring

--- top.ml
+++ top.ml
@@ -1,6 +1,6 @@
 (* #use this to setup the interactive environment. *)
 
-#require "cstruct, cstruct.syntax, sexplib.syntax, nocrypto, stringext";;
+#require "cstruct, cstruct.syntax, sexplib.syntax, nocrypto, astring";;
 
 #directory "_build/src";;
 #load_rec "otr.cmo";;
